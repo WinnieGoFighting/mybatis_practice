c54d792e9c89a139553d79bd97b34e1e838ee4fe
WinnieGoFighting <winnie20100910@gmail.com>
2020-05-12 15:09:38 -0500

    6 commit mybatis_05_level2CacheDefault mybatis_05_level2CacheEhCache

---
+++ mybatis_05_level2CacheDefault/src/test/java/TestQuery.java
@@ -0,0 +1,31 @@
+
+import com.ni.mybatis.domain.Teacher;
+import com.ni.mybatis.mapper.TeacherMapper;
+import com.ni.mybatis.util.MyBatisUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class TestQuery {
+
+    /**
+        保存两个老师，两个学生
+     */
+    @Test
+    public void testSave() throws Exception {
+        SqlSession session = MyBatisUtil.getSession();
+        TeacherMapper tMapper = session.getMapper(TeacherMapper.class);
+        tMapper.get(1l);
+        session.close();
+        System.out.println("---------------------------------------------");
+        session = MyBatisUtil.getSession();
+        tMapper = session.getMapper(TeacherMapper.class);
+        tMapper.get(1l);
+        session.close();
+
+    }
+
+
+}
---
+++ mybatis_05_level2CacheEhCache/src/main/java/com/ni/mybatis/mapper/TeacherMapper.java
@@ -0,0 +1,11 @@
+package com.ni.mybatis.mapper;
+
+import com.ni.mybatis.domain.Teacher;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface TeacherMapper {
+    Teacher get(Long id);
+
+}
